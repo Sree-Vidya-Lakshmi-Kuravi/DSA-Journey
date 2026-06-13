4cc3a5de97a2959b107a6029692921335bc2715a
Sree Vidya Lakshmi Kuravi <vidyakuravi@gmail.com>
2026-06-13 21:26:39 +0530

    Used frequency counter for finding most frequent element

---
+++ Week 1/Day_3/most_freq_ele.py
@@ -0,0 +1,14 @@
+### Finding the most frequent element
+
+arr = [1, 2, 2, 3, 2, 1]
+
+freq = {}
+for i in arr:
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1
+
+sorted_arr = sorted(freq.items(), key = lambda x: -x[1])
+print(sorted_arr[0][0], 'occured', sorted_arr[0][1], 'times')
+print("Sorted array:", sorted_arr)

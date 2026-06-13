a1d2baec71bb35cac9f39264a32d0048eed0c009
Sree Vidya Lakshmi Kuravi <vidyakuravi@gmail.com>
2026-06-13 21:08:09 +0530

    Used linear search logic

--- Week 1/Day_3/count_target_occurrences.py
+++ Week 1/Day_3/count_target_occurrences.py
@@ -1,19 +1,31 @@
-### Counting occurrences of a target
+# ### Counting occurrences of a target
+
+# arr = [1, 2, 2, 3, 2]
+# target = 2
+
+# freq = {}
+
+# for i in arr:
+#     if i == target:
+#         if i in freq:
+#             freq[i] += 1
+#         else:
+#             freq[i] = 1
+
+# for i, j in freq.items():
+#     print("Number of times target occurred:", j)
+
+
+
+def count_occur(arr, target):
+    t_count = 0
+    for i in arr:
+        if i == target:
+            t_count += 1
+
+    print("Target Count:", t_count) 
 
 arr = [1, 2, 2, 3, 2]
 target = 2
 
-freq = {}
-
-for i in arr:
-    if i == target:
-        if i in freq:
-            freq[i] += 1
-        else:
-            freq[i] = 1
-
-for i, j in freq.items():
-    print("Number of times target occurred:", j)
-
-
-
+count_occur(arr, target)

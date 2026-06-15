245bbc343e235332d72de3927d25d877e615b791
Sree Vidya Lakshmi Kuravi <vidyakuravi@gmail.com>
2026-06-15 19:59:04 +0530

    Worked on hollow square pattern

--- Week 1/Day_4/star_patterns.py
+++ Week 1/Day_4/star_patterns.py
@@ -173,4 +173,15 @@ def butterfly(n):
         for j in range(n - i - 1):
             print('*', end = '')
         print()
-butterfly(5)
+# butterfly(5)
+
+## Hollow square
+def hollow_square(n):
+    for i in range(n):
+        for j in range(n):
+            if i == 0 or i == n-1 or j == 0 or j == n-1:
+                print("*", end = " ")
+            else:
+                print(" ", end = " ")
+        print()
+# hollow_square(4)

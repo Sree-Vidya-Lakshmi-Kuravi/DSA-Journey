5b235c88e6654984ef293cfee914842a2546c7c2
Sree Vidya Lakshmi Kuravi <vidyakuravi@gmail.com>
2026-06-16 14:35:22 +0530

    Worked on concentric matrix pattern

--- Week 1/Day_4/num_patterns.py
+++ Week 1/Day_4/num_patterns.py
@@ -75,3 +75,40 @@ def increase_num(n):
             num += 1
         print()
 # increase_num(5)
+
+## Half butterfly using numbers
+def half_butterfly(n):
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            print(j, end = " ")
+        for j in range((n-i)*2, 0, -1):
+            print(" ", end = " ")
+        for j in range(i, 0, -1):
+            print(j, end = " ")
+        print()
+# half_butterfly(4)
+
+
+# 4 4 4 4 4 4 4
+# 4 3 3 3 3 3 4
+# 4 3 2 2 2 3 4
+# 4 3 2 1 2 3 4
+# 4 3 2 2 2 3 4
+# 4 3 3 3 3 3 4
+# 4 4 4 4 4 4 4 
+
+def square_num(n):
+    size = 2 * n - 1
+    for i in range(size): 
+        for j in range(size):
+            top = i  # row
+            left = j # column
+            bottom = size - 1 - i
+            right = size - 1 - j
+
+            layer = min(top, bottom, left, right)
+            value = n - layer
+
+            print(value, end = " ")
+        print()
+# square_num(4)

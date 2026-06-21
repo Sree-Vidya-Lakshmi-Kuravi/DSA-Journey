d7d878187cf78325e727eafd2d16260406f7ad26
Sree Vidya Lakshmi Kuravi <vidyakuravi@gmail.com>
2026-06-21 11:51:54 +0530

    Worked on reversing an array using recursion

--- Week 1/Day_6/reverse_array.py
+++ Week 1/Day_6/reverse_array.py
@@ -17,5 +17,17 @@ def rev_Array(arr: list):
 
     return arr
     print(rev_Array(arr))
-    
-print(rev_Array([1, 2, 3, 4, 5, 7, 67, 90, 32]))
+
+# print(rev_Array([1, 2, 3, 4, 5, 7, 67, 90, 32]))
+
+
+def rev_arr(arr: list, l, r):
+    arr[l], arr[r] = arr[r], arr[l]
+    l += 1
+    r -= 1
+    if l >= r:
+        return arr
+    return rev_arr(arr, l, r)
+
+arr = [3,4,5,6]
+print(rev_arr(arr, 0, len(arr) - 1))

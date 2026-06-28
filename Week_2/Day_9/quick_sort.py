f069a38cb13c829c713488ec25a7a7fb8d549441
Sree Vidya Lakshmi Kuravi <vidyakuravi@gmail.com>
2026-06-28 16:56:38 +0530

    Implemented Quick Sort using pivot as the first element

--- Week_2/Day_9/quick_sort.py
+++ Week_2/Day_9/quick_sort.py
@@ -1,5 +1,7 @@
 ## Quick Sort
 
+## Using pivot as the last element
+
 def partition(arr, low, high):
     pivot = arr[high]
     i = low - 1  # initially, no element is smaller
@@ -19,7 +21,50 @@ def quickSort(arr, low, high):
         quickSort(arr, low, pivot_index - 1) # left sub array
         quickSort(arr, pivot_index + 1, high) # right sub array
 
+arr = [4, 3, 2, 1]
+# quickSort(arr, 0, len(arr) - 1)
+# print(arr) 
+
+
+
+### Using pivot as first element
+
+def partition(arr, low, high):
+    pivot = arr[low]
+    left = low + 1  # position
+    right = high   # position
+
+    while True:
+        # Move until the left element is lesser than pivot
+        while left <= right and arr[left] < pivot:
+            left += 1
+        
+        # Move until the right element is greater than pivot
+        while left <= right and arr[right] > pivot:
+            right -= 1
+
+        if left > right:
+            break
+
+        # Swap the elements
+        arr[left], arr[right] = arr[right], arr[left]
+
+    # Place the pivot in the correct place
+    arr[low], arr[right] = arr[right], arr[low]
+
+    return right
+
+def quickSort(arr, low, high):
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        quickSort(arr, low, pivot_index - 1) 
+        quickSort(arr, pivot_index + 1, high) 
 
 arr = [4, 3, 2, 1]
 quickSort(arr, 0, len(arr) - 1)
 print(arr) 
+
+
+
+
+
